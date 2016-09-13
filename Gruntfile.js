0cbbbf358d41a79fcb27dea82d5b415894c7db68
The Doans <thedoans@Thes-MacBook-Air.local>
2016-09-13 13:53:26 -0700

    Adds for new files

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports= function(grunt){
+	
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		'min': {
+			'dist': {
+				'options': {
+					'report': false
+				},
+				'files': [{
+					'src': ['js/night-mode.js'],
+					'dest': 'js/app.js'
+				}]
+			}
+		},
+
+		'htmlmin': {                                     // Task
+			dist: {                                      // Target
+				options: {                                 // Target options
+					removeComments: true,
+					collapseWhitespace: true,
+					minifyJS: true
+				},
+				files: {                                   // Dictionary of files
+					'index.html': 'index-original.html',     // 'destination': 'source'
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-yui-compressor');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+	grunt.registerTask('default', ['min', 'htmlmin']);
+
+}
